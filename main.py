0a38db5333b4b4690cae30c58a441637683dd016
Daria <dasha130407daria@gmail.com>
2025-05-01 20:45:08 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from factorial_recursive import factorial_recursive
+from fibonacci_recursive import fibonacci_recursive
+from sum_list_recursive import sum_list_recursive
+from is_palindrome_recursive import is_palindrome_recursive
+
+print("Факторіал 5:", factorial_recursive(5))
+print("7-ме число Фібоначчі:", fibonacci_recursive(7))
+print("Сума списку [1, 2, 3, 4]:", sum_list_recursive([1, 2, 3, 4]))
+print("Чи є паліндромом 'А роза упала на лапу Азора':", is_palindrome_recursive("А роза упала на лапу Азора"))
